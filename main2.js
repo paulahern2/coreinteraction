0a9ec9225a47c898bc7916d3ba4babffe44ebbca
paulahern2 <42781123+paulahern2@users.noreply.github.com>
2019-05-08 18:10:02 -0400

    exercise for lab

---
+++ main2.js
@@ -0,0 +1,18 @@
+var newyorkTemp = 63
+var irelandTemp = 48
+
+function f2c(fahrenheit) {
+	var newyorkTemp = fahrenheit
+}
+	var f2c = (newyorkTemp - 32) + 5/9
+
+	function f2c2(fahrenheit2) {
+		var irelandTemp = fahrenheit2
+	}
+
+var f2c2 = (irelandTemp - 32) + 5/9
+
+var currentTemp = "The temperature in New York is "+ f2c + "\xB0C at 6:01pm"
+console.log(currentTemp)
+var currentTemp = "The temperature in Ireland is "+ f2c + "\xB0C at 6:07pm"
+console.log(currentTemp)
