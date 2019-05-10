5b086c29c3851fa76a88ce5fd109765169006129
paulahern2 <42781123+paulahern2@users.noreply.github.com>
2019-05-10 10:53:58 -0400

    weathersite final before crit

--- myscript.js
+++ myscript.js
@@ -418,3 +418,14 @@ $(document).ready(function(){
         //END OF MYWEATHERINTERPRETATION FUNCTION
     }
 });
+
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
